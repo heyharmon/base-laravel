da243d67c00aeb948477c53bf05107e5db8882d4
Ryan Harmon <heyharmon@gmail.com>
2025-06-20 19:48:23 -0600

    Add conversation API and chat frontend

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,9 @@ Route::get('/colors', function () {
         'orange',
     ];
 });
+
+use App\Http\Controllers\Api\ConversationController;
+
+Route::post('/conversations', [ConversationController::class, 'store']);
+Route::get('/conversations/{conversation}/chats', [ConversationController::class, 'chats']);
+Route::post('/conversations/{conversation}/messages', [ConversationController::class, 'sendMessage']);
